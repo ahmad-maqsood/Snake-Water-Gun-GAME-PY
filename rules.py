4686735d8e19a727f7de1d14ad5d4d1a321af3c8
Ahmad Maqsood <am9566222@gmail.com>
2026-02-10 07:49:13 +0500

    Delete rules.py

--- rules.py
+++
@@ -1,8 +0,0 @@
-'''
-The Battle Rules:-
-
-Snake vs. Water: Snake wins (The snake drinks the water).
-Water vs. Gun: Water wins (The water ruins the gun's mechanism).
-Gun vs. Snake: Gun wins (The gun shoots the snake).
-
-'''
